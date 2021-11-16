0dd574ebe7437c1db44d6d36df51135346980051
Ali Rezaei <ali.rezaei09@gmail.com>
2021-11-17 00:42:24 +0330

    Add Hilt to buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,10 +11,12 @@ object PluginsVersions {
     const val GRADLE_ANDROID = "7.0.3"
     const val KOTLIN = "1.5.31"
     const val NAVIGATION = "2.3.5"
+    const val HILT = "2.40"
 }
 
 dependencies {
     implementation("com.android.tools.build:gradle:${PluginsVersions.GRADLE_ANDROID}")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${PluginsVersions.KOTLIN}")
     implementation("androidx.navigation:navigation-safe-args-gradle-plugin:${PluginsVersions.NAVIGATION}")
+    implementation("com.google.dagger:hilt-android-gradle-plugin:${PluginsVersions.HILT}")
 }
--- buildSrc/src/main/java/commons/android-library.gradle.kts
+++ buildSrc/src/main/java/commons/android-library.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("kotlin-android")
     id("kotlin-parcelize")
     id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
