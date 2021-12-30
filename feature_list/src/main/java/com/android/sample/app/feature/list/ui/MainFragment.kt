99a53c624ec6ae8ccb7876c51343c081c35d13a9
Ali Rezaei <ali.rezaei09@gmail.com>
2021-12-30 23:21:33 +0330

    RoundedCornerShape for Toolbar

--- feature_list/src/main/java/com/android/sample/app/feature/list/ui/MainFragment.kt
+++ feature_list/src/main/java/com/android/sample/app/feature/list/ui/MainFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
@@ -52,11 +55,15 @@ class MainFragment : Fragment() {
             topBar = {
                 TopAppBar(
                     title = {
-                        Column(modifier = Modifier.padding(4.dp)) {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
                             Text(text = stringResource(id = R.string.disney))
                         }
                     },
                     elevation = 8.dp,
+                    modifier = Modifier.clip(RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp))
                 )
             },
             content = {
