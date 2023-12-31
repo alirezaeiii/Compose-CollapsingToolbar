73df33079aa96b01fcf765d873dd9ddc9798d75c
Ali Rezaei <ali.rezaei09@gmail.com>
2023-12-31 14:43:57 -0800

    Rename

--- app/src/main/java/com/android/sample/app/Application.kt
+++ app/src/main/java/com/android/sample/app/Application.kt
@@ -1,11 +1,10 @@
 package com.android.sample.app
 
-import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class BaseApplication : Application() {
+class Application : android.app.Application() {
 
     override fun onCreate() {
         super.onCreate()
