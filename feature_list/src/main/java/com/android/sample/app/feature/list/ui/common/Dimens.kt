d319cc8fb8dd9bf00b644aca5380e13d9e05c08b
Ali Rezaei <ali.rezaei09@gmail.com>
2024-01-08 03:29:10 -0800

    Cleanup

--- feature_list/src/main/java/com/android/sample/app/feature/list/ui/common/Dimens.kt
+++ feature_list/src/main/java/com/android/sample/app/feature/list/ui/common/Dimens.kt
@@ -7,9 +7,6 @@ import com.android.sample.app.feature.list.R
 
 object Dimens {
 
-    val DetailAppBarHeight: Dp
-        @Composable get() = dimensionResource(R.dimen.detail_app_bar_height)
-
     val DetailBoxImageHeight: Dp
         @Composable get() = dimensionResource(R.dimen.detail_box_image_height)
 }
